e80a9287b6fc653f495decba4261364bb515a1ca
Natan Krol <natankrol5@gmail.com>
2022-11-15 01:13:21 +0100

    normal/high priority button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {Initial} from './initial.js';
 const rightSide = document.querySelector('.right-side');
+const leftSide = document.querySelector('.left-side');
+
 
 
 
@@ -11,15 +13,20 @@ const newTodoBtn = document.querySelector('.new-todo-btn');
 let list = [];
 
 
+const  todoFormContainer = document.createElement('div');
+        todoFormContainer.classList.add('todo-form-container');
+        todoFormContainer.classList.add('todo-form-container-hidden');
+
+        rightSide.appendChild(todoFormContainer);
+        todoFormContainer.innerHTML = '<form class="todo-form"><button type="button" class="close-form-btn">X</button><label for="title">Title:</label><input type="text" id="title" required><label for="description">Description:</label><input type="text" id="description" required><label for="dueDate">Due Date:</label><input type="date" id="duedate" required></label><label for="priority">Priority:</label><select id="priority" class="priority-select"><option id="normal" value="Normal Priority">Normal</option><option id ="high" value="High Priority">High Priority</option></select><input type="submit" class="submit-btn"></form>';
+
 
 
 
 newTodoBtn.addEventListener('click', function(){
-    const todoFormContainer = document.createElement('div');
-    todoFormContainer.classList.add('todo-form-container');
 
-    rightSide.appendChild(todoFormContainer);
-    todoFormContainer.innerHTML = '<form class="todo-form"><button type="button" class="close-form-btn">X</button><label for="title">Title:</label><input type="text" id="title"><label for="description">Description:</label><input type="text" id="description"><label for="dueDate">Due Date:</label><input type="date" id="duedate"></label><label for="priority">Priority:</label><select id="priority" class="priority-select"><option value="normal">Normal</option><option value="high">High Priority</option></select><input type="submit" class="submit-btn"></form>';
+    todoFormContainer.classList.remove('todo-form-container-hidden');
+    
 
     let todoForm = document.querySelector('.todo-form');
 
@@ -34,7 +41,6 @@ newTodoBtn.addEventListener('click', function(){
 
 
     todoForm.addEventListener('submit', function(e){
-        
         let title = todoForm.querySelector('#title').value;
         let description = todoForm.querySelector('#description').value;
         let dueDate = todoForm.querySelector('#duedate').value;
@@ -44,6 +50,7 @@ newTodoBtn.addEventListener('click', function(){
         let newTask = Todo(title, description, dueDate, priority, id);
         list.push(newTask);
         e.preventDefault();
+        e.stopImmediatePropagation();
         todoForm.reset();
 
         let newTaskTitle = newTask.title;
@@ -52,15 +59,14 @@ newTodoBtn.addEventListener('click', function(){
         let newTaskPriority = newTask.priority;
         let newTaskId = newTask.Id;
 
-        mainSectionList.innerHTML += `<li class="list-item" id=${newTaskId}><h1 class="task-title">${newTaskTitle}</h1><p class="class-description">${newTaskDescription}</p></li>`
+
+        mainSectionList.innerHTML += `<li class="list-item" id=${newTaskId}><h1 class="task-title">${newTaskTitle}</h1><p class="task-description">${newTaskDescription}</p><p class ="due-date">${newTaskDueDate}</p><p class="task-priority">${newTaskPriority}</p></li>`
 
 
         list.forEach((item, i) => {
             item.id = i + 1;
         }
         );
-
-        // populateTodo()
 
         console.log(list);
 
@@ -81,13 +87,46 @@ newTodoBtn.addEventListener('click', function(){
 let mainSection = document.createElement('div');
 rightSide.appendChild(mainSection);
 mainSection.classList.add('main-section');
-mainSection.innerHTML = '<ul class="main-section-list"><li>To Do</li></ul>';
+mainSection.innerHTML = '<ul class="main-section-list"></ul>';
 
 const mainSectionList = document.querySelector('.main-section-list');
 
-function populateTodo(){
-    list.forEach(function(li){
-        mainSectionList.innerHTML += `<li id="${li.id}">${li.title}</li>`
+
+
+
+
+// Normal priority button
+
+let normalPriorityButton = document.createElement('button');
+normalPriorityButton.classList.add('normal-priority-button');
+normalPriorityButton.innerHTML = 'Normal Priority';
+leftSide.appendChild(normalPriorityButton);
+
+normalPriorityButton.addEventListener('click', function(){
+    let normalPriorityTasks = list.filter(function(el){
+        if (el.priority === "Normal Priority"){
+            return el
+        }
     })
-}
+    console.log(normalPriorityTasks);
+
+
+
+    let normalPrioritySection = document.createElement('div');
+    rightSide.removeChild(mainSection);
+    rightSide.appendChild(normalPrioritySection;)
+    normalPrioritySection.classList.add('normal-priority-section');
+    normalPrioritySection.innerHTML = '<ul class="normal-priority-section-list"></ul>';
+    let normalPrioritySectionList = document.querySelector('.normal-priority-section-list');
+    normalPrioritySection.forEach((item) => {
+        let li = document.createElement('li');
+        li.innerText = `<li class="list-item" id=${item.id}><h1 class="task-title">${item.title}</h1><p class="task-description">${item.description}</p><p class ="due-date">${item.dueDate}</p><p class="task-priority">${item.priority}</p></li>`;
+        normalPrioritySectionList.appendChild(li);
+    })
+
+})
+
+
+
+
 
--- src/initial.js
+++ src/initial.js
@@ -2,11 +2,11 @@ export const Initial = () => {
     const leftSide = document.querySelector('.left-side');
     const rightSide = document.querySelector('.right-side');
     const today = document.createElement('div');
-    today.innerHTML = '<h1>Today</h1>';
+    today.innerHTML = '<h1>All</h1>';
     leftSide.appendChild(today);
 
     const newTodoBtn = document.createElement('button');
-    newTodoBtn.innerHTML = '<p>New Todo</p>';
+    newTodoBtn.innerHTML = '<p>New Task</p>';
     newTodoBtn.classList.add('new-todo-btn');
     rightSide.append(newTodoBtn);
 }
